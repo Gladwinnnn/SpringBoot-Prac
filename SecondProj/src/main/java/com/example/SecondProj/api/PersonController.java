ebcfe3b6a4d52233587228cbf0769f2c6c29a018
Gladwinnnn <gladwin1503@hotmail.com>
2020-12-27 17:16:46 +0800

    finish up basics

--- SecondProj/src/main/java/com/example/SecondProj/api/PersonController.java
+++ SecondProj/src/main/java/com/example/SecondProj/api/PersonController.java
@@ -1,7 +1,14 @@
 package com.example.SecondProj.api;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +33,23 @@ public class PersonController {
 		personService.addPerson(person);
 	}
 	
+	@GetMapping("/getallpeople")
+	public List<Person> getAllPeople(){
+		return personService.getAllPeople();
+	}
+	
+	@GetMapping("/getpersonbyid/{id}")
+	public Person getPersonById(@PathVariable("id") UUID id) {
+		return personService.getPersonById(id).orElse(null);
+	}
+	
+	@DeleteMapping("deletepersonbyid/{id}")
+	public void deletePerson(@PathVariable("id") UUID id) {
+		personService.deletePersonById(id);
+	}
+	
+	@PutMapping("updatepersonbyid/{id}")
+	public void updatePerson(@PathVariable("id") UUID id, @RequestBody Person person) {
+		personService.updatePersonById(id, person);
+	}
 }
--- SecondProj/src/main/java/com/example/SecondProj/service/PersonService.java
+++ SecondProj/src/main/java/com/example/SecondProj/service/PersonService.java
@@ -1,5 +1,9 @@
 package com.example.SecondProj.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -25,5 +29,21 @@ public class PersonService {
 
 	public int addPerson(Person person) {
 		return personDao.insertPerson(person);
+	}
+	
+	public List<Person> getAllPeople(){
+		return personDao.selectAllPeople();
+	}
+	
+	public Optional<Person> getPersonById(UUID id){
+		return personDao.selectPersonById(id);
+	}
+	
+	public int deletePersonById(UUID id) {
+		return personDao.deletePersonById(id);
+	}
+	
+	public int updatePersonById(UUID id, Person person) {
+		return personDao.updatePersonById(id, person);
 	}
 }
